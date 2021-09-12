4541eaf895792f1cf2896805064d33320d4d59ca
Dan Pock <self@danpock.me>
2021-09-12 15:20:41 -0400

    add a command specific manage sections trait

---
+++ src/Console/CommandManagesSections.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MallardDuck\LaravelTraits\Console;
+
+use MallardDuck\LaravelTraits\Generic\ManagesLayoutSections;
+
+trait CommandManagesSections
+{
+    use ManagesLayoutSections;
+}
